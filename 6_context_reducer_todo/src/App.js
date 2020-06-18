7bb1f9dd61d4089831d802291ef46e29adda2e7e
GauravPandey892 <892dgauravpandey@gmail.com>
2020-06-18 17:33:09 +0530

    added explanation of reducer in comments

--- 6_context_reducer_todo/src/App.js
+++ 6_context_reducer_todo/src/App.js
@@ -7,6 +7,7 @@ import {TodoContext} from './context/TodoContext';
 import todoReducer from './context/reducer';
 import TodoForm from './Components/TodoForm';
 import Todos from './Components/Todos';
+import { findAllByDisplayValue } from '@testing-library/react';
 
 
 const App = () => {
@@ -24,3 +25,20 @@ const App = () => {
 }
 
 export default App;
+
+// usereducer just like usestate is used for state management
+
+// Reduce functions
+// The reduce  method executes a reducer function(that you provide)
+// on each element of the Array, resulting in a single output value.
+
+// reduce method takes two parameters 
+// first is reducer function, second the intital value
+// which reducer can make use of 
+// reducer method accepts two parameters again
+// reduces them to a single value and returns that value
+
+// useReducer(reducer, initialState)
+// newState = reducer(currentState, action)
+// useReducer returns a pair of values 
+// [newState, dispatch]
